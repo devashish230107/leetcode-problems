52da9a55c443cd284972d24fb11a87d00d0360dd
Devashish <devashish230107@gmail.com>
2026-08-10 20:35:47 +0530

    Time: 7 ms (20.66%), Space: 10.8 MB (80.45%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char, int> hash;
-        string ans;
         for (int i = 0; i < s.length(); i++) {
             hash[s[i]]++;
         }
-        char maxChar;
-        int maxVal = 0;
-        while (!hash.empty()) {
-            char maxChar;
-            int maxVal = 0;
-            for (auto it : hash) {
-                if (it.second > maxVal) {
-                    maxVal = it.second;
-                    maxChar = it.first;
-                }
+        vector<pair<char, int>> v;
+        for (auto it : hash) {
+            v.push_back({it.first, it.second});
+        }
+        sort(v.begin(), v.end(),
+             [](auto& a, auto& b) { return a.second > b.second; });
+        string ans;
+        for (auto it : v) {
+            for (int i = 0; i < it.second; i++) {
+                ans += it.first;
             }
-            for (int i = 0; i < maxVal; i++) {
-                ans += maxChar;
-            }
-            hash.erase(maxChar);
         }
         return ans;
     }
