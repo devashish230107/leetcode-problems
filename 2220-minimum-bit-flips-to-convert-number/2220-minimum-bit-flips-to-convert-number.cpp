6090163ed4ab97e34662d59277b6f7ab74a2a1ca
Devashish <devashish230107@gmail.com>
2026-09-26 20:45:05 +0530

    Time: 0 ms (100%), Space: 8 MB (25.4%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while (n > 1) {
+            if (n % 2 == 1) {
+                count++;
+            }
+            n = n / 2;
+        }
+        if (n == 1) {
+            count++;
+        }
+        return count;
+    }
+    int minBitFlips(int start, int goal) { return hammingWeight(start ^ goal); }
+};
